3f41604f50680e664765ed0a72f8bb9dea945393
ravliuk <vivexpro@gmail.com>
2025-12-09 04:19:16 +0200

    add db and cache values to settings

--- settings.py
+++ settings.py
@@ -2,5 +2,17 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    DB_HOST: str = "localhost"
+    DB_PORT: str = "5432"
+    DB_USER: str = "postgres"
+    DB_PASSWORD: str = "password"
+    DB_NAME: str = "pomodoro"
+
+    CACHE_HOST: str = "localhost"
+    CACHE_PORT: int = 6379
+    CACHE_DB: int = 0
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
-    DATABASE_URL: str = "postgresql+psycopg2://postgres:password@localhost:5432/pomodoro"
+    DATABASE_URL: str = (
+        "postgresql+psycopg2://postgres:password@localhost:5432/pomodoro"
+    )
